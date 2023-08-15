045dedfe78b46f66542453d7e0d58fb225954e56
Michael Baumgartner <michbaum@ethz.ch>
2023-08-15 11:37:55 +0200

    Add drift plotting script.

---
+++ utils/plot_drift_errors.py
@@ -0,0 +1,62 @@
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_translation_error(csv_file):
+    # Load the CSV file into a pandas DataFrame
+    data = pd.read_csv(csv_file)
+
+    # Convert the 'Timestamp' column to datetime format
+    # data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Plot TranslationError over Timestamp
+    sns.lineplot(data=data, x='Timestamp', y='TranslationError', label='Translation Error')
+
+    # Add title and labels
+    plt.title('Translation Error Over Time')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Translation Error (meters)')
+
+    # Rotate x-axis labels for better visibility
+    plt.xticks(rotation=45)
+
+    # Display the plot
+    plt.tight_layout()
+    plt.show()
+
+def plot_angle_error(csv_file):
+    # Load the CSV file into a pandas DataFrame
+    data = pd.read_csv(csv_file)
+
+    # Convert the 'Timestamp' column to datetime format
+    # data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Plot AngleError over Timestamp
+    sns.lineplot(data=data, x='Timestamp', y='AngleError', color='orange', label='Angle Error')
+
+    # Add title and labels
+    plt.title('Angle Error Over Time')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Angle Error (radians)')
+
+    # Rotate x-axis labels for better visibility
+    plt.xticks(rotation=45)
+
+    # Display the plot
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <csv_file>")
+    else:
+        csv_file = sys.argv[1]
+        plot_translation_error(csv_file)
+        plot_angle_error(csv_file)
---
+++ utils/plot_multiple_drift_errors.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_translation_errors(data_list, names):
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Plot TranslationError over Timestamp for each data file
+    for data, name in zip(data_list, names):
+        sns.lineplot(data=data, x='Timestamp', y='TranslationError', label=name)
+
+    # Add title and labels
+    plt.title('Translation Error Over Time')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Translation Error (meters)')
+
+    # Rotate x-axis labels for better visibility
+    plt.xticks(rotation=45)
+
+    # Display the plot
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+def plot_angle_errors(data_list, names):
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Plot AngleError over Timestamp for each data file
+    for data, name in zip(data_list, names):
+        sns.lineplot(data=data, x='Timestamp', y='AngleError', label=name)
+
+    # Add title and labels
+    plt.title('Angle Error Over Time')
+    plt.xlabel('Timestamp')
+    plt.ylabel('Angle Error (radians)')
+
+    # Rotate x-axis labels for better visibility
+    plt.xticks(rotation=45)
+
+    # Display the plot
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <folder_path>")
+    else:
+        folder_path = sys.argv[1]
+        
+        data_list = []
+        names = []
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".csv"):
+                csv_file = os.path.join(folder_path, filename)
+                data = pd.read_csv(csv_file)
+                data_list.append(data)
+                # Extract the 1st, 4th and 5th word from the filename split by '_'
+                filename = filename.split('.')[0]
+                filename = '_'.join([filename.split('_')[0], filename.split('_')[3], filename.split('_')[4]])
+                names.append(filename)
+
+        plot_translation_errors(data_list, names)
+        plot_angle_errors(data_list, names)
